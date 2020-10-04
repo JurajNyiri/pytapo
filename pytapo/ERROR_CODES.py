f6f30345f4a0821b6f98ce628b34597ba2147649
Juraj Nyíri <jnyiri@sk.ibm.com>
2020-10-04 18:52:53 +0200

    Add: More discovered error code meanings

--- pytapo/ERROR_CODES.py
+++ pytapo/ERROR_CODES.py
@@ -2,5 +2,8 @@ ERROR_CODES = {
     "-40401": "Invalid stok value",
     "-64324": "Privacy mode is ON, not able to execute",
     "-64302": "Preset ID not found",
-    "-64321": "Preset ID was deleted so no longer exists"
+    "-64321": "Preset ID was deleted so no longer exists",
+    "-40106": "Parameter to get/do does not exist",
+    "-40105": "Method does not exist",
+    "-40101": "Parameter to set does not exist"
 }
