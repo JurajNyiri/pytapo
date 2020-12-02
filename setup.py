4939dbac034e0871b325cbfb22d234828d119a47
Davide Depau <davide@depau.eu>
2020-12-02 20:46:24 +0100

    Add test dependencies

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     url="https://github.com/JurajNyiri/pytapo",
     packages=setuptools.find_packages(),
     install_requires=["requests", "urllib3", "pycryptodome"],
+    tests_require=["pytest", "pytest-asyncio", "mock"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
