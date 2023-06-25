9ce4f2c662892ab32df2f2fdc2128b1e1b883bc2
Antoine Boucher <antoine.bou13@gmail.com>
2023-06-24 22:49:29 -0400

    Update downloader.py

--- pytapo/media_stream/downloader.py
+++ pytapo/media_stream/downloader.py
@@ -1,10 +1,10 @@
-from pytapo.media_stream.convert import Convert
-from pytapo import Tapo
-from datetime import datetime
-
+import hashlib
 import json
 import os
-import hashlib
+from datetime import datetime
+
+from pytapo import Tapo
+from pytapo.media_stream.convert import Convert
 
 
 class Downloader:
@@ -16,30 +16,20 @@ class Downloader:
         startTime: int,
         endTime: int,
         outputDirectory="./",
-        padding=None,
+        padding=5,
         overwriteFiles=None,
-        window_size=None,  # affects download speed, with higher values camera sometimes stops sending data
+        window_size=200,  # affects download speed, with higher values camera sometimes stops sending data
         fileName=None,
     ):
         self.tapo = tapo
         self.startTime = startTime
         self.endTime = endTime
-        self.padding = padding
+        self.padding = int(padding)
         self.fileName = fileName
-        self.scriptStartTime = int(
-            datetime.now().timestamp()
-        )  # keeps track of when was class initiated
-        if padding is None:
-            self.padding = 5
-        else:
-            self.padding = int(padding)
-
+        self.scriptStartTime = int(datetime.now().timestamp())
         self.outputDirectory = outputDirectory
         self.overwriteFiles = overwriteFiles
-        if window_size is None:
-            self.window_size = 200
-        else:
-            self.window_size = int(window_size)
+        self.window_size = int(window_size)
 
     def md5(self, fileName):
         if os.path.isfile(fileName):
@@ -51,154 +41,72 @@ class Downloader:
         return False
 
     async def downloadFile(self, callbackFunc=None):
-        if callbackFunc is not None:
-            callbackFunc("Starting download")
+        callbackFunc = callbackFunc or (lambda msg: None)
+        callbackFunc("Starting download")
         async for status in self.download():
-            if callbackFunc is not None:
-                callbackFunc(status)
-            pass
-        if callbackFunc is not None:
-            callbackFunc("Finished download")
-
-        md5Hash = self.md5(status["fileName"])
-
-        status["md5"] = "" if md5Hash is False else md5Hash
-
+            callbackFunc(status)
+        callbackFunc("Finished download")
+        status["md5"] = self.md5(status["fileName"]) or ""
         return status
 
-    async def download(self, retry=False):
+    async def download(self):
         downloading = True
+        retry = False
         while downloading:
-            # todo: add a way to not download recent videos to prevent videos in progress
-            dateStart = datetime.utcfromtimestamp(int(self.startTime)).strftime(
-                "%Y-%m-%d %H_%M_%S"
-            )
-            dateEnd = datetime.utcfromtimestamp(int(self.endTime)).strftime(
-                "%Y-%m-%d %H_%M_%S"
-            )
-            segmentLength = self.endTime - self.startTime
-            if self.fileName is None:
-                fileName = (
-                    self.outputDirectory + str(dateStart) + "-" + dateEnd + ".mp4"
-                )
-            else:
-                fileName = self.outputDirectory + self.fileName
-            if self.scriptStartTime - self.FRESH_RECORDING_TIME_SECONDS < self.endTime:
-                currentAction = "Recording in progress"
-                yield {
-                    "currentAction": currentAction,
-                    "fileName": fileName,
-                    "progress": 0,
-                    "total": 0,
-                }
-                downloading = False
-            elif os.path.isfile(fileName):
-                currentAction = "Skipping"
-                yield {
-                    "currentAction": currentAction,
-                    "fileName": fileName,
-                    "progress": 0,
-                    "total": 0,
-                }
-                downloading = False
-            else:
-                convert = Convert()
-                mediaSession = self.tapo.getMediaSession()
-                if retry:
-                    mediaSession.set_window_size(50)
-                else:
-                    mediaSession.set_window_size(self.window_size)
-                async with mediaSession:
-                    payload = {
-                        "type": "request",
-                        "seq": 1,
-                        "params": {
-                            "playback": {
-                                "client_id": self.tapo.getUserID(),
-                                "channels": [0, 1],
-                                "scale": "1/1",
-                                "start_time": str(self.startTime),
-                                "end_time": str(self.endTime),
-                                "event_type": [1, 2],
-                            },
-                            "method": "get",
-                        },
-                    }
+            yield from self._handle_download(retry)
+            downloading = False
+            retry = not retry
 
-                    payload = json.dumps(payload)
-                    dataChunks = 0
-                    if retry:
-                        currentAction = "Retrying"
-                    else:
-                        currentAction = "Downloading"
-                    downloadedFull = False
-                    async for resp in mediaSession.transceive(payload):
-                        if resp.mimetype == "video/mp2t":
-                            dataChunks += 1
-                            convert.write(resp.plaintext, resp.audioPayload)
-                            detectedLength = convert.getLength()
-                            if detectedLength is False:
-                                yield {
-                                    "currentAction": currentAction,
-                                    "fileName": fileName,
-                                    "progress": 0,
-                                    "total": segmentLength,
-                                }
-                                detectedLength = 0
-                            else:
-                                yield {
-                                    "currentAction": currentAction,
-                                    "fileName": fileName,
-                                    "progress": detectedLength,
-                                    "total": segmentLength,
-                                }
-                            if (detectedLength > segmentLength + self.padding) or (
-                                retry
-                                and detectedLength
-                                >= segmentLength  # fix for the latest latest recording
-                            ):
-                                downloadedFull = True
-                                currentAction = "Converting"
-                                yield {
-                                    "currentAction": currentAction,
-                                    "fileName": fileName,
-                                    "progress": 0,
-                                    "total": 0,
-                                }
-                                convert.save(fileName, segmentLength)
-                                downloading = False
-                                break
-                    if downloading:
-                        # Handle case where camera randomly stopped respoding
-                        if not downloadedFull and not retry:
-                            currentAction = "Retrying"
-                            yield {
-                                "currentAction": currentAction,
-                                "fileName": fileName,
-                                "progress": 0,
-                                "total": 0,
-                            }
-                            retry = True
-                        else:
-                            detectedLength = convert.getLength()
-                            if (
-                                detectedLength >= segmentLength - 5
-                            ):  # workaround for weird cases where the recording is a bit shorter than reported
-                                downloadedFull = True
-                                currentAction = "Converting [shorter]"
-                                yield {
-                                    "currentAction": currentAction,
-                                    "fileName": fileName,
-                                    "progress": 0,
-                                    "total": 0,
-                                }
-                                convert.save(fileName, segmentLength)
-                            else:
-                                currentAction = "Giving up"
-                                yield {
-                                    "currentAction": currentAction,
-                                    "fileName": fileName,
-                                    "progress": 0,
-                                    "total": 0,
-                                }
-                            downloading = False
+    def _handle_download(self, retry=False):
+        dateStart = datetime.utcfromtimestamp(int(self.startTime)).strftime("%Y-%m-%d %H_%M_%S")
+        dateEnd = datetime.utcfromtimestamp(int(self.endTime)).strftime("%Y-%m-%d %H_%M_%S")
+        segmentLength = self.endTime - self.startTime
+        fileName = (self.outputDirectory + str(dateStart) + "-" + dateEnd + ".mp4") if self.fileName is None else self.outputDirectory + self.fileName
+        convert = Convert()
+        mediaSession = self.tapo.getMediaSession()
+        mediaSession.set_window_size(50 if retry else self.window_size)
+        async with mediaSession:
+            payload = self._prepare_payload()
+            dataChunks = 0
+            downloadedFull = False
+            async for resp in mediaSession.transceive(payload):
+                if resp.mimetype == "video/mp2t":
+                    dataChunks += 1
+                    convert.write(resp.plaintext, resp.audioPayload)
+                    detectedLength = convert.getLength() or 0
+                    yield self._create_status("Retrying" if retry else "Downloading", fileName, detectedLength, segmentLength)
+                    if (detectedLength > segmentLength + self.padding) or (retry and detectedLength >= segmentLength):
+                        downloadedFull = True
+                        yield self._create_status("Converting", fileName)
+                        convert.save(fileName, segmentLength)
+                        break
+            if not downloadedFull and not retry:
+                yield self._create_status("Retrying", fileName)
+                retry = True
+            elif detectedLength := convert.getLength():
+                if detectedLength >= segmentLength - 5:
+                    downloadedFull = True
+                    yield self._create_status("Converting [shorter]", fileName)
+                    convert.save(fileName, segmentLength)
+                else:
+                    yield self._create_status("Giving up", fileName)
+
+    def _prepare_payload(self):
+        return json.dumps({
+            "type": "request",
+            "seq": 1,
+            "params": {
+                "playback": {
+                    "client_id": self.tapo.getUserID(),
+                    "channels": [0, 1],
+                    "scale": "1/1",
+                    "start_time": str(self.startTime),
+                    "end_time": str(self.endTime),
+                    "event_type": [1, 2],
+                },
+                "method": "get",
+            },
+        })
+
+    def _create_status(self, action, fileName, progress=0, total=0):
+        return {"currentAction": action, "fileName": fileName, "progress": progress, "total": total}
