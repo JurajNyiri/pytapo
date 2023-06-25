3a72e23d7837d03adad95b4229ec54e58936d747
Antoine Boucher <antoine.bou13@gmail.com>
2023-06-24 22:52:34 -0400

    Update response.py

--- pytapo/media_stream/response.py
+++ pytapo/media_stream/response.py
@@ -1,19 +1,31 @@
-from typing import Mapping, Optional
-
+from typing import Dict, Optional, Any
 
 class HttpMediaResponse:
-    def __init__(
-        self,
-        seq: Optional[int],
-        session: Optional[int],
-        headers: Mapping[str, str],
-        encrypted: bool,
-        mimetype: str,
-        ciphertext: Optional[bytes],
-        plaintext: bytes,
-        audioPayload: bytes,
-        json_data,
-    ):
+    """
+    This class encapsulates the HTTP response from a media server.
+    
+    Attributes:
+        seq (int): The sequence number of the response.
+        session (int): The session identifier.
+        headers (Dict[str, str]): The HTTP headers of the response.
+        encrypted (bool): Indicates whether the response is encrypted.
+        mimetype (str): The MIME type of the response.
+        ciphertext (Optional[bytes]): The encrypted payload if present.
+        plaintext (bytes): The decrypted payload.
+        json_data (Any): The json data extracted from the response.
+        audioPayload (bytes): The audio data extracted from the response.
+    """
+    
+    def __init__(self,
+                 seq: int,
+                 session: int,
+                 headers: Dict[str, str],
+                 encrypted: bool,
+                 mimetype: str,
+                 ciphertext: Optional[bytes],
+                 plaintext: bytes,
+                 json_data: Any,
+                 audioPayload: bytes) -> None:
         self.seq = seq
         self.session = session
         self.headers = headers
