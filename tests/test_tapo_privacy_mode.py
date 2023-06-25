348d52fb1aa9d61eeb32fe195035006dd539957b
Antoine Boucher <antoine.bou13@gmail.com>
2023-06-24 22:38:28 -0400

    Update test_tapo_privacy_mode.py

--- tests/test_tapo_privacy_mode.py
+++ tests/test_tapo_privacy_mode.py
@@ -1,1 +1,13 @@
+import pytest
+from tapo import Tapo
+import time
 
+host = "your_host"
+user = "your_user"
+password = "your_password"
+
+def test_getPrivacyMode():
+    # your code here
+
+def test_setPrivacyMode():
+    # your code here
