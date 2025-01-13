1af97a6014ad33dc62a966dff475005cb9658b9c
Juraj Nyíri <juraj.nyiri@gmail.com>
2025-01-13 11:24:46 +0100

    Update: Bump version

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ version=$(awk -F'"' '/version=/{print $2}' "$setup_script")
 echo "Setting $version in version.py..."
 echo "PYTAPO_VERSION = '$version'" > pytapo/version.py
 rm -rf dist/*
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload --repository pytapo dist/*
+python3.10 setup.py sdist bdist_wheel
+python3.10 -m twine upload --repository pytapo dist/*
