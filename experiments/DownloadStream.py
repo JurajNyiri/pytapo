b0ea6a5793b65d07d20c8cfe74f4b85531404ce8
Juraj Nyíri <juraj.nyiri@gmail.com>
2025-03-03 13:38:52 +0100

    Update: Better example for DownloadStream.py experiment

--- experiments/DownloadStream.py
+++ experiments/DownloadStream.py
@@ -7,6 +7,9 @@ from pytapo.media_stream.streamer import Streamer
 outputDir = os.environ.get("OUTPUT")  # directory path where videos will be saved
 host = os.environ.get("HOST")  # change to camera IP
 password_cloud = os.environ.get("PASSWORD_CLOUD")  # set to your cloud password
+stream_port = os.environ.get("STREAM_PORT")
+control_port = os.environ.get("CONTROL_PORT")
+enable_audio = os.environ.get("ENABLE_AUDIO")
 
 # optional
 window_size = os.environ.get(
@@ -14,24 +17,34 @@ window_size = os.environ.get(
 )  # set to prefferred window size, affects download speed and stability, recommended: 50
 
 print("Connecting to camera...")
-tapo = Tapo(host, "admin", password_cloud, password_cloud)
+tapo = Tapo(
+    host,
+    "admin",
+    password_cloud,
+    password_cloud,
+    controlPort=control_port,
+    streamPort=stream_port,
+)
 
 
 def callback(status):
     print(status)
+    pass
 
 
-keepRunningFor = 10
+keepRunningFor = 6000
 
 
 async def download_async():
     print("Getting recordings...")
     ranFor = 0
-    downloader = Streamer(tapo, callback, outputDir)
-    await downloader.start_hls()
+    downloader = Streamer(
+        tapo, callback, outputDir, includeAudio=True if enable_audio == "yes" else False
+    )
+    pids = await downloader.start_hls()
+    print(pids)
 
     while True:
-        print("sleeping")
         ranFor += 1
         await asyncio.sleep(1)  # Use asyncio.sleep()
         if ranFor > keepRunningFor:
