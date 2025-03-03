35e82789de7c4e2e864f930dc1888dace362b674
Juraj Nyíri <juraj.nyiri@gmail.com>
2025-03-03 18:41:35 +0100

    Update: Clear build folder on deploy

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ version=$(awk -F'"' '/version=/{print $2}' "$setup_script")
 echo "Setting $version in version.py..."
 echo "PYTAPO_VERSION = '$version'" > pytapo/version.py
 rm -rf dist/*
+rm -rf build/*
 python3.10 setup.py sdist bdist_wheel
 python3.10 -m twine upload --repository pytapo dist/*
