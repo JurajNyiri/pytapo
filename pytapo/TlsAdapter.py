e0ec82c6e21c24a84e870c0a7cbbc1591f3bbe84
Juraj Nyíri <juraj.nyiri@gmail.com>
2025-10-20 20:55:40 +0200

    Fix #158: Add ECDHE-ECDSA-AES128-GCM-SHA256 to allowed certificates

--- pytapo/TlsAdapter.py
+++ pytapo/TlsAdapter.py
@@ -5,12 +5,7 @@ import urllib3
 from urllib3.util import ssl_
 from urllib3.poolmanager import PoolManager
 
-CIPHERS = ":".join(
-    [
-        "AES256-SHA",
-        "AES128-GCM-SHA256",
-    ]
-)
+CIPHERS = ":".join(["AES256-SHA", "AES128-GCM-SHA256", "ECDHE-ECDSA-AES128-GCM-SHA256"])
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
